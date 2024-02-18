82ecac199eda5d0d68a0618d0e5655fe9dbdd2b8
AJay <andrewli707577@gmail.com>
2024-02-18 20:53:05 +0800

    feat(login): add login view

---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import { z } from 'zod';
+import { OInput } from '@/components/shared';
+
+const validationSchema = toTypedSchema(z.object({
+  account: z.string().min(6),
+}));
+
+const { handleSubmit } = useForm({
+  validationSchema,
+});
+
+const onSubmit = handleSubmit((values) => {
+  console.log(JSON.stringify(values, null, 2));
+});
+</script>
+
+<template>
+  <main>Login</main>
+  <QCard>
+    <QCardSection>
+      <OInput name="account" />
+    </QCardSection>
+    <QCardActions>
+      <QBtn label="Submit" @click="onSubmit" />
+    </QCardActions>
+  </QCard>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
