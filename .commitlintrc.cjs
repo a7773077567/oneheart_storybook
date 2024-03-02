c79d09932c6abac221a5f382275b782212f37876
AJay <andrewli707577@gmail.com>
2024-03-02 21:11:48 +0800

    chore: add header-max-length for 100 characters of commitlint

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,3 +1,7 @@
+// 0 -> disable
+// 1 -> warning
+// 2 -> error
 module.exports = { extends: ['@commitlint/config-angular'], rules: {
   'type-enum': [2, 'always', ['chore', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+  'header-max-length': [2, 'always', 100],
 } };
