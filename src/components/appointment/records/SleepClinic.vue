6612663c7c05525a6657ab3dc5e0080a56ea4670
Hoo <sherryho148@gmail.com>
2024-04-23 20:10:00 +0800

    feat(schedule): create sleep clinic page

--- src/components/appointment/records/SleepClinic.vue
+++ src/components/appointment/records/SleepClinic.vue
@@ -1,11 +1,51 @@
 <script setup lang="ts">
+import type { ClientSchedule } from '@/api';
 
+defineProps<{
+  scheduleId: number;
+  scheduleDetail: ClientSchedule;
+}>();
 </script>
 
 <template>
-  sleepClinic
+  <div class="sleep_record">
+    <p class="q-mb-md">
+      {{ scheduleDetail.date }}
+    </p>
+    <form>
+      <fieldset>
+        <legend>客戶問題描述</legend>
+        <OInput name="personalHealthStatus" type="textarea" class="input__item" hide-bottom-space />
+      </fieldset>
+      <fieldset>
+        <legend>評估狀態</legend>
+        <OInput name="nutritionistAdvice" type="textarea" class="input__item" hide-bottom-space />
+      </fieldset>
+      <fieldset>
+        <legend>產品描述</legend>
+        <OInput name="nutritionistAdvice" type="textarea" class="input__item" hide-bottom-space />
+      </fieldset>
+      <fieldset>
+        <legend>備註</legend>
+        <OInput name="nutritionistAdvice" type="textarea" class="input__item" hide-bottom-space />
+      </fieldset>
+      <fieldset>
+        <OFile name="attachment" label="選擇檔案" @update:model-value="" />
+      </fieldset>
+    </form>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+.nusleepecord {
+  form > fieldset {
+    legend {
+      margin-bottom: 4px;
+      font-size: 14px;
+    }
+    & + fieldset {
+      margin-top: 16px;
+    }
+  }
+}
 </style>
