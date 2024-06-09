c538665f5eb2623d169dd1b9fa4f7576578d015b
Hoo <sherryho148@gmail.com>
2024-06-09 15:24:37 +0800

    feat(shared): DatePicker component add range prop

--- src/components/shared/DatePicker.vue
+++ src/components/shared/DatePicker.vue
@@ -3,18 +3,26 @@ import { computed } from 'vue';
 import dayjs from 'dayjs';
 import { useField } from 'vee-validate';
 
+interface MultiRange { from: string; to: string };
+
 interface Props {
   modelValue?: string;
   name?: string;
+  range?: boolean;
 }
 
 const props = defineProps<Props>();
 
-const { value: fieldValue } = useField<string>(() => props.name || '', undefined, {
+const { value: fieldValue } = useField<MultiRange | string>(() => props.name || '', undefined, {
   syncVModel: true,
 });
 
-const currentDate = computed(() => fieldValue.value ? dayjs(fieldValue.value).format('YYYY年M月D日') : '');
+const currentDate = computed<string>(() => {
+  if (props.range && fieldValue.value && ('from' in (fieldValue.value as object)) && 'to' in (fieldValue.value as object))
+    return `${dayjs((fieldValue.value as MultiRange)?.from).format('YYYY年M月D日') ?? dayjs()} - ${dayjs((fieldValue.value as MultiRange)?.to ?? dayjs()).format('YYYY年M月D日')}`;
+
+  return fieldValue.value ? dayjs(fieldValue.value as string).format('YYYY年M月D日') : '';
+});
 </script>
 
 <template>
@@ -22,7 +30,7 @@ const currentDate = computed(() => fieldValue.value ? dayjs(fieldValue.value).fo
     <span class="date-picker__label">{{ currentDate }}</span>
     <QIcon name="o_calendar_month" size="28px" class="cursor-pointer">
       <QPopupProxy cover transition-show="scale" transition-hide="scale">
-        <QDate v-model="fieldValue" mask="YYYY-MM-DD" today-btn>
+        <QDate v-model="fieldValue" mask="YYYY-MM-DD" today-btn :range="range">
           <div class="row items-center justify-end">
             <QBtn v-close-popup label="Close" color="primary" flat />
           </div>
