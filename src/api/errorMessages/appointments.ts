7d6381b2f429b8d4b984dfcd8d07e36429a09413
AJay <andrewli707577@gmail.com>
2024-07-17 17:34:37 +0800

    feat(appointment): add error-message for duplicated group appoinemnt

--- src/api/errorMessages/appointments.ts
+++ src/api/errorMessages/appointments.ts
@@ -2,6 +2,7 @@ const messages: [string, string][] = [
   ['userShiftAppointment is already booked', '客戶於此時段已預約過'],
   ['clientSchedule can\'t be rearrange more than one time', '無法預約改期，預約改期最多只能一次'],
   ['ClientGroup points not enough', '群組點數不足'],
+  ['client already in groupClass', '客戶已預約過此堂團課'],
 ];
 
 export default messages;
