d93e42936a4eefb28b0d7ec77cca4aec91a06adb
AJay <andrewli707577@gmail.com>
2024-07-23 14:27:53 +0800

    refactor(comp): add number type for o-input

--- src/components/shared/OInput.vue
+++ src/components/shared/OInput.vue
@@ -24,6 +24,30 @@ const { value, errorMessage } = useField<string>(() => props.name || '', props.c
       {{ label }}
     </div>
     <QInput
+      v-if="type === 'number'"
+      v-model.number="value"
+      :error="!!errorMessage"
+      :error-message="errorMessage"
+      dense
+      outlined
+      :disable="disable"
+      :type="type"
+    >
+      <template #append>
+        <QIcon v-if="dateMode" name="o_calendar_month" size="28px" class="cursor-pointer">
+          <QPopupProxy cover transition-show="scale" transition-hide="scale">
+            <QDate v-model="value" mask="YYYY-MM-DD" today-btn>
+              <div class="row items-center justify-end">
+                <QBtn v-close-popup label="Close" color="primary" flat />
+              </div>
+            </QDate>
+          </QPopupProxy>
+        </QIcon>
+        <slot v-else name="append" />
+      </template>
+    </QInput>
+    <QInput
+      v-else
       v-model="value"
       :error="!!errorMessage"
       :error-message="errorMessage"
