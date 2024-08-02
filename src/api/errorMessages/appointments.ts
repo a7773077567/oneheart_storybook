6932153cb5266fd9bd73ae83221ebd22702e6392
Hoo <sherryho148@gmail.com>
2024-08-02 13:11:35 +0800

    feat(appointment): add multiChannelPay error msg

--- src/api/errorMessages/appointments.ts
+++ src/api/errorMessages/appointments.ts
@@ -3,6 +3,7 @@ const messages: [string, string][] = [
   ['clientSchedule can\'t be rearrange more than one time', '無法預約改期，預約改期最多只能一次'],
   ['ClientGroup points not enough', '群組點數不足'],
   ['client already in groupClass', '客戶已預約過此堂團課'],
+  ['multiChannelPay sum is not equal to amount', '交易總金額不符'],
 ];
 
 export default messages;
