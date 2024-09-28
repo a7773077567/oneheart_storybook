d7a4c803bb4351f563d3826ff96870d2c262bed6
Hoo <sherryho148@gmail.com>
2024-09-28 15:30:14 +0800

    feat(point): #310, point multi-pay add methods

--- src/components/order/point/TopupCheckout.vue
+++ src/components/order/point/TopupCheckout.vue
@@ -24,7 +24,7 @@ const userStore = useUserStore();
 const clientStore = useClientStore();
 const isCheckoutOpen = ref(false);
 const payments = ref<Payments>([]);
-const methodOptions = Object.values(PaymentMethods).filter(payment => payment.forPointAndGroup).map(({ label, identifier }) => ({ label, value: identifier }));
+const methodOptions = Object.values(PaymentMethods).map(({ label, identifier }) => ({ label, value: identifier }));
 
 const purchaseDetail = computed<CheckTableData>(() => [
   { key: 'date', value: dayjs().format('YYYY-MM-DD'), span: true, custom: true },
