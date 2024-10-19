7ee633e7a14a5b4578f1883361814eb60dfec074
AJay <andrewli707577@gmail.com>
2024-10-19 21:24:12 +0800

    fix(appointment): adjust label wording of finish-record

--- src/components/appointment/records/NutritionClinic.vue
+++ src/components/appointment/records/NutritionClinic.vue
@@ -84,7 +84,7 @@ async function finishRecord() {
     </form>
     <div class="nutrition_form_action">
       <QBtn label="儲存" outline style="width: 127px" @click="onSubmit" />
-      <QBtn v-if="scheduleState === '完成服務'" label="完成病例" color="primary" style="width: 127px;" @click="finishRecord" />
+      <QBtn v-if="scheduleState === '完成服務'" 病例完成 color="primary" style="width: 127px;" @click="finishRecord" />
     </div>
   </div>
 </template>
