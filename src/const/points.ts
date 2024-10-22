f874845832f3d7336a913fb38fce3adc227968b4
Hoo <sherryho148@gmail.com>
2024-10-22 23:08:08 +0800

    feat(point): #447, new points plans

--- src/const/points.ts
+++ src/const/points.ts
@@ -5,7 +5,7 @@ interface PointsPlan {
   paidPointGained: number;
   giftPointGained: number;
   price: number;
-  for?: PointTypes[];
+  for: PointTypes[];
 }
 export const POINTS_PLAN: { [counting: number]: PointsPlan } = {
   1: {
@@ -16,42 +16,42 @@ export const POINTS_PLAN: { [counting: number]: PointsPlan } = {
     for: [PointTypes.物理治療],
   },
   2: {
-    name: '$20000：10堂送1堂',
+    name: '$20,000：10堂送1堂',
     paidPointGained: 10,
     giftPointGained: 1,
     price: 20000,
     for: [PointTypes.物理治療],
   },
   3: {
-    name: '$14500：5堂',
+    name: '$14,500：5堂',
     paidPointGained: 5,
     giftPointGained: 0,
     price: 14500,
     for: [PointTypes.院長物理治療],
   },
   4: {
-    name: '$30000：10堂送1堂',
+    name: '$30,000：10堂送1堂',
     paidPointGained: 10,
     giftPointGained: 1,
     price: 30000,
     for: [PointTypes.院長物理治療],
   },
   5: {
-    name: '$13500：12堂',
+    name: '$13,500：12堂',
     paidPointGained: 12,
     giftPointGained: 0,
     price: 13500,
     for: [PointTypes.營養],
   },
   6: {
-    name: '$16500：10堂送1堂',
+    name: '$16,500：10堂送1堂',
     paidPointGained: 10,
     giftPointGained: 1,
     price: 16500,
     for: [PointTypes.教練課],
   },
   7: {
-    name: '$45000：30堂送1堂',
+    name: '$45,000：30堂送1堂',
     paidPointGained: 30,
     giftPointGained: 1,
     price: 45000,
@@ -65,151 +65,88 @@ export const POINTS_PLAN: { [counting: number]: PointsPlan } = {
     for: [PointTypes.物理治療, PointTypes.院長物理治療, PointTypes.教練課, PointTypes.營養],
   },
   9: {
-    name: '試營運：初診3堂$5550',
-    paidPointGained: 3,
-    giftPointGained: 0,
-    price: 5550,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  10: {
-    name: '試營運：初診5堂$8800',
-    paidPointGained: 5,
-    giftPointGained: 0,
-    price: 8800,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  11: {
-    name: '試營運：初診10堂$18700',
-    paidPointGained: 10,
+    name: '矯正方案$18,700',
+    paidPointGained: 1,
     giftPointGained: 0,
     price: 18700,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
   },
-  12: {
-    name: '試營運：複診5堂$9300',
+  10: {
+    name: '軍警消醫護5堂 $7,500',
     paidPointGained: 5,
     giftPointGained: 0,
-    price: 9300,
+    price: 7500,
+    for: [PointTypes.物理治療, PointTypes.院長物理治療],
+  },
+  11: {
+    name: '軍警消醫護10堂 $15,000',
+    paidPointGained: 10,
+    giftPointGained: 0,
+    price: 15000,
+    for: [PointTypes.物理治療, PointTypes.院長物理治療],
+  },
+  12: {
+    name: 'winback能量治療五堂 $9,500',
+    paidPointGained: 5,
+    giftPointGained: 0,
+    price: 9500,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
   },
   13: {
-    name: '試營運：複診10堂$19360',
+    name: 'winback能量治療十堂送一 $20,000',
     paidPointGained: 10,
-    giftPointGained: 0,
-    price: 19360,
+    giftPointGained: 1,
+    price: 20000,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
   },
   14: {
-    name: '試營運：初診單堂$1500',
-    paidPointGained: 1,
+    name: '教練課5堂 $8,000',
+    paidPointGained: 5,
     giftPointGained: 0,
-    price: 1500,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
+    price: 8000,
+    for: [PointTypes.教練課],
   },
   15: {
-    name: '試營運：複診單堂$2000',
-    paidPointGained: 1,
+    name: '藝文初診５堂$8,850',
+    paidPointGained: 5,
     giftPointGained: 0,
-    price: 2000,
+    price: 8850,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
   },
   16: {
-    name: '試營運：初診單堂$1400',
-    paidPointGained: 1,
+    name: '藝文複診５堂$9,250',
+    paidPointGained: 5,
     giftPointGained: 0,
-    price: 1400,
+    price: 9250,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
   },
   17: {
-    name: '試營運：初診5堂$8750',
-    paidPointGained: 5,
+    name: '藝文初診10堂$17,200',
+    paidPointGained: 10,
     giftPointGained: 0,
-    price: 8700,
+    price: 17200,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
   },
   18: {
-    name: '試營運：初診10堂$17000',
+    name: '藝文複診10堂$17,500',
     paidPointGained: 10,
     giftPointGained: 0,
-    price: 17000,
+    price: 17500,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
   },
   19: {
-    name: '試營運：初診20堂$33400',
+    name: '藝文初診20堂$33,400',
     paidPointGained: 20,
     giftPointGained: 0,
     price: 33400,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
   },
   20: {
-    name: '試營運：複診單堂$1900',
-    paidPointGained: 1,
-    giftPointGained: 0,
-    price: 1900,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  21: {
-    name: '試營運：複診5堂$9250',
-    paidPointGained: 5,
-    giftPointGained: 0,
-    price: 9250,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  22: {
-    name: '試營運：複診10堂$17500',
-    paidPointGained: 10,
-    giftPointGained: 0,
-    price: 17500,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  23: {
-    name: '試營運：複診20堂$34000',
+    name: '藝文複診20堂$34,000',
     paidPointGained: 20,
     giftPointGained: 0,
     price: 34000,
     for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  24: {
-    name: '試營運：矯正方案$18700',
-    paidPointGained: 1,
-    giftPointGained: 0,
-    price: 18700,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  25: {
-    name: '軍警消醫護5堂 $7500',
-    paidPointGained: 5,
-    giftPointGained: 0,
-    price: 7500,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  26: {
-    name: '軍警消醫護10堂 $15000',
-    paidPointGained: 10,
-    giftPointGained: 0,
-    price: 15000,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  27: {
-    name: 'winback能量治療五堂 $9500',
-    paidPointGained: 5,
-    giftPointGained: 0,
-    price: 9500,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  28: {
-    name: 'winback能量治療十堂送一 $20000',
-    paidPointGained: 10,
-    giftPointGained: 1,
-    price: 20000,
-    for: [PointTypes.物理治療, PointTypes.院長物理治療],
-  },
-  29: {
-    name: '教練課5堂 $8000',
-    paidPointGained: 5,
-    giftPointGained: 0,
-    price: 80000,
-    for: [PointTypes.教練課],
   },
 } as const;
 
@@ -220,41 +157,14 @@ interface TypePlan {
 
 }
 
-const physicalRelatedPlans = Array(20).fill(9).map((plan, idx) => plan + idx);
+const getPlanByType = (plan: PointTypes) => Object.keys(POINTS_PLAN).map(plan => +plan).filter(planNumber => POINTS_PLAN[+planNumber as keyof typeof POINTS_PLAN].for?.includes(plan));
+
 export const plansByType: TypePlan[] = [
-  { id: 1, type: PointTypes.物理治療, plans: [1, 2, 8, ...physicalRelatedPlans] },
-  { id: 2, type: PointTypes.院長物理治療, plans: [3, 4, 8, ...physicalRelatedPlans] },
-  { id: 3, type: PointTypes.營養, plans: [5, 8, ...physicalRelatedPlans] },
-  { id: 4, type: PointTypes.教練課, plans: [6, 7, 8, ...physicalRelatedPlans, 29] },
+  { id: 1, type: PointTypes.物理治療, plans: getPlanByType(PointTypes.物理治療) },
+  { id: 2, type: PointTypes.院長物理治療, plans: getPlanByType(PointTypes.院長物理治療) },
+  { id: 3, type: PointTypes.營養, plans: getPlanByType(PointTypes.營養) },
+  { id: 4, type: PointTypes.教練課, plans: getPlanByType(PointTypes.教練課),
+  },
 ];
 
-// 方案總表
-// 1: '9500元：5堂',
-// 2: '20000元：10堂送1堂',
-// 3: '14500元：5堂',
-// 4: '30000元：10堂送1堂',
-// 5: '13500元：12堂',
-// 6: '16500：10堂送1堂',
-// 7: '45000：30堂送1堂',
-// 8: '開帳包：0堂',
-// 9. 試營運初診3堂5550
-// 10. 試營運初診5堂8800
-// 11. 試營運初診10堂18700
-// 12. 試營運複診5堂9300
-// 13. 試營運複診10堂19360
-// 14. 試營運初診單堂1500
-// 15. 試營運複診單堂2000
-// 16. 開幕初診單堂1400
-// 17. 開幕初診5堂8750
-// 18. 開幕初診10堂17000
-// 19. 開幕初診20堂33400
-// 20. 開幕複診單堂1900
-// 21. 開幕複數5堂9250
-// 22. 開幕複診10堂17500
-// 23. 開幕複診20堂34000
-// 24. 體態矯正方案18700
-// 25. 軍警消醫護5堂 $7500
-// 26. 軍警消醫護10堂 $15000
-// 27. winback能量治療五堂 $9500
-// 28. winback能量治療十堂送一 $20000
-// 29. 教練課5堂 $8000
+// 方案總表: https://www.notion.so/enginelin/faf06bae4bbf4069baba2c5c9ee54146?pvs=4
