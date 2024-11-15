c453241c96f88277e016ce24529b69b44547cd3f
Hoo <sherryho148@gmail.com>
2024-11-15 21:46:10 +0800

    fix: ts error

--- src/components/shared/OMemberSearch.vue
+++ src/components/shared/OMemberSearch.vue
@@ -113,8 +113,7 @@ function removeValue() {
         + 新增 {{ customValue }}
       </QItem>
     </template>
-    <!-- @ts-expect-error -->
-    <template v-for="slotname in Object.keys($slots)" #[slotname]>
+    <template v-for="slotname in Object.keys($slots) as string[]" #[slotname]>
       <slot :name="slotname" />
     </template>
   </QSelect>
