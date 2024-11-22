84595e227dab99c620f5b1e8dfb26d947f75bd6f
AJay <andrewli707577@gmail.com>
2024-11-22 21:27:04 +0800

    feat(appointment): add auto-recommendation display of appointment-card and client-info

--- src/components/appointment/records/ClientInfo.vue
+++ src/components/appointment/records/ClientInfo.vue
@@ -46,6 +46,7 @@ const data = computed(() => {
     { key: 'name', label: '姓名', value: client.value.name },
     { key: 'isFirstClientSchedule', label: '初診', value: schedule.value.isFirstClientSchedule ? '初診' : '複診' },
     { key: 'isEmployeePrice', label: '員工價', value: schedule.value.isEmployeePrice },
+    { key: 'autoRecommendation', label: '自動推薦', value: schedule.value.isUsingAutoRecommend },
     { key: 'lineId', label: 'LINE ID', value: client.value.lineUserId },
     { key: 'liffIntroducerName', label: '介紹人', value: client.value.liffIntroducerName ?? '未填寫' },
     { key: 'phone', label: '電話', value: client.value.phone },
@@ -209,6 +210,11 @@ async function handleEmployeePriceChange(state: boolean) {
             </div>
           </div>
         </template>
+        <template #autoRecommendation="{ row }">
+          <QIcon v-if="row.value" name="check_circle" color="green" class="q-mr-sm" />
+          <span>{{ row.value ? '是（選擇自動推薦治療師）' : '否' }}</span>
+        </template>
+
         <template #lineId="{ row }">
           <div v-if="row.value">{{ row.value }}</div>
           <QBadge v-else color="red-1" text-color="red-10" class="text-weight-bold q-mx-sm">LINE 未綁定</QBadge>
@@ -219,9 +225,7 @@ async function handleEmployeePriceChange(state: boolean) {
             <div class="liffIntroducerName__value">
               後台綁定
               <template v-if="!client.introducer">
-                <QBadge color="red-1" text-color="red-10" class="text-weight-bold q-mx-sm">
-                  介紹人未綁定
-                </QBadge>
+                <QBadge color="red-1" text-color="red-10" class="text-weight-bold q-mx-sm">介紹人未綁定</QBadge>
                 <a class="link" @click="$router.push({ name: 'clientInfo', params: { clientId: scheduleDetail.clientId } })">前往綁定</a>
               </template>
               <template v-else>
