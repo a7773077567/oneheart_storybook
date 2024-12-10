0e11404182f8bd56ca05c91cf270386701a51b93
AJay <andrewli707577@gmail.com>
2024-12-10 15:00:37 +0800

    fix(dashboard): hide dashboard for admin temporarily

--- src/views/home/AdminDashboard.vue
+++ src/views/home/AdminDashboard.vue
@@ -1,67 +1,68 @@
 <script setup lang="ts">
-import { CaseStatusMap, RangeSelectOptions } from '@/const/dashboard';
-import { TherapistOverview } from '@/components/home/dashboard';
-import { computed, ref } from 'vue';
-import { useAppointmentStore, useShiftStore, useUserStore } from '@/stores';
-import { LineChart } from '@/components/shared';
-import { useReceptionStore } from '@/stores/home/dashboard/reception';
+// import { CaseStatusMap, RangeSelectOptions } from '@/const/dashboard';
+// import { TherapistOverview } from '@/components/home/dashboard';
+// import { computed, ref } from 'vue';
+// import { useAppointmentStore, useShiftStore, useUserStore } from '@/stores';
+// import { LineChart } from '@/components/shared';
+// import { useReceptionStore } from '@/stores/home/dashboard/reception';
 
-const userStore = useUserStore();
-const shiftStore = useShiftStore();
-const receptionStore = useReceptionStore();
-const appointmentStore = useAppointmentStore();
-await appointmentStore.getUsers([userStore.currentSpaceId!]);
-await receptionStore.getTodayBusinessStatus();
+// const userStore = useUserStore();
+// const shiftStore = useShiftStore();
+// const receptionStore = useReceptionStore();
+// const appointmentStore = useAppointmentStore();
+// await appointmentStore.getUsers([userStore.currentSpaceId!]);
+// await receptionStore.getTodayBusinessStatus();
 
-// Therapist Overview
-const caseStatusData = Object.values(CaseStatusMap).reduce((acc: any, item, idx) => {
-  acc.labels = [...(acc.label ?? []), item.label];
-  acc.data = [...(acc.data ?? []), 5 + (idx * 3)];
-  acc.backgroundColor = [...(acc.backgroundColor ?? []), item.color];
-  return acc;
-}, {});
+// // Therapist Overview
+// const caseStatusData = Object.values(CaseStatusMap).reduce((acc: any, item, idx) => {
+//   acc.labels = [...(acc.label ?? []), item.label];
+//   acc.data = [...(acc.data ?? []), 5 + (idx * 3)];
+//   acc.backgroundColor = [...(acc.backgroundColor ?? []), item.color];
+//   return acc;
+// }, {});
 
-const caseStatusInfoData = Object.values(CaseStatusMap).map((item, idx) => {
-  const { color, label } = item;
-  return {
-    color,
-    label,
-    values: [
-      `${5 + (idx * 3)}件`,
-      `${+(((5 + (idx * 3)) / 55).toFixed(2)) * 100}%`,
-    ],
-  };
-});
+// const caseStatusInfoData = Object.values(CaseStatusMap).map((item, idx) => {
+//   const { color, label } = item;
+//   return {
+//     color,
+//     label,
+//     values: [
+//       `${5 + (idx * 3)}件`,
+//       `${+(((5 + (idx * 3)) / 55).toFixed(2)) * 100}%`,
+//     ],
+//   };
+// });
 
-const caseStatus = computed(() => {
-  return {
-    title: '病例狀態',
-    subtitle: '總時數 28 hr',
-    chartData: caseStatusData,
-    infoData: caseStatusInfoData,
-  };
-});
+// const caseStatus = computed(() => {
+//   return {
+//     title: '病例狀態',
+//     subtitle: '總時數 28 hr',
+//     chartData: caseStatusData,
+//     infoData: caseStatusInfoData,
+//   };
+// });
 
-const typeOptions = computed(() => shiftStore.spaceShiftOptions);
-const therapistSelect = ref(0);
-const therapistRangeSelect = ref(0);
+// const typeOptions = computed(() => shiftStore.spaceShiftOptions);
+// const therapistSelect = ref(0);
+// const therapistRangeSelect = ref(0);
 
-const therapistTypeSelect = ref(typeOptions.value.map(option => option.value));
-const therapistSelectOptions = [
-  {
-    label: '所有治療師',
-    value: 0,
-  },
-  ...appointmentStore.activeUsers,
-];
+// const therapistTypeSelect = ref(typeOptions.value.map(option => option.value));
+// const therapistSelectOptions = [
+//   {
+//     label: '所有治療師',
+//     value: 0,
+//   },
+//   ...appointmentStore.activeUsers,
+// ];
 
-const lineLabels = ['1', '2', '3'];
-const lineData = [100, 200, 300];
+// const lineLabels = ['1', '2', '3'];
+// const lineData = [100, 200, 300];
 </script>
 
 <template>
   <div class="dashboard">
-    <TherapistOverview
+    dashboard
+    <!-- <TherapistOverview
       v-model:therapistSelect="therapistSelect"
       v-model:rangeSelect="therapistRangeSelect"
       v-model:typeSelect="therapistTypeSelect"
@@ -72,7 +73,7 @@ const lineData = [100, 200, 300];
       :checkout-plan="caseStatus"
     />
 
-    <LineChart :labels="lineLabels" :data="lineData" />
+    <LineChart :labels="lineLabels" :data="lineData" /> -->
   </div>
 </template>
 
