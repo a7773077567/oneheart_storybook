bcddbc89641ec2a97c11a435bb2e363b2b8d29d6
AJay <andrewli707577@gmail.com>
2025-01-21 14:28:36 +0800

    feat(layout): #849 #850 colors & typography system for ui-2.0

--- vite.config.ts
+++ vite.config.ts
@@ -23,4 +23,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/scss/variables" as *;
+        @use "@/assets/scss/mixins" as *;`,
+      },
+    },
+  },
 });
