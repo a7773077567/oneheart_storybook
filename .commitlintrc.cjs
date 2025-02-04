cedeba90379101d297354456e9839d7106d12bb3
AJay <andrewli707577@gmail.com>
2025-02-04 11:32:06 +0800

    chore: add hotfix type enum for commitlint

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -2,6 +2,6 @@
 // 1 -> warning
 // 2 -> error
 module.exports = { extends: ['@commitlint/config-angular'], rules: {
-  'type-enum': [2, 'always', ['chore', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+  'type-enum': [2, 'always', ['chore', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'hotfix']],
   'header-max-length': [2, 'always', 150],
 } };
