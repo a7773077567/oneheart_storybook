a9ddc73aa94db81144cdf9470200ca019c205293
Hoo <sherryho148@gmail.com>
2025-04-20 19:38:13 +0800

    feat(pointnbonus): create points and bonus page

---
+++ src/views/home/pointsNBonus/Index.vue
@@ -0,0 +1,41 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+
+const tabs = [
+  { name: 'googleReview', label: 'Google 評論管理' },
+  { name: 'educationPoints', label: '教育積分' },
+];
+const currentTab = ref(tabs[0].name);
+</script>
+
+<template>
+  <div class="points_n_bonus">
+    <QTabs
+      v-model="currentTab"
+      align="left"
+      dense
+      active-color="primary"
+    >
+      <QTab
+        v-for="(tab, idx) in tabs"
+        :key="idx"
+        :name="tab.name"
+        :label="tab.label"
+        class="q-py-sm"
+        style="text-transform: initial"
+        @click="$router.push({ name: tab.name })"
+      />
+    </QTabs>
+    <div class="points_n_bonus--page">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.points_n_bonus {
+  &--page {
+    padding: 18px 24px;
+  }
+}
+</style>
