fcec45ee80f84aba3693318966b250a65a5b8743
AJay <andrewli707577@gmail.com>
2025-05-10 22:23:48 +0800

    feat(component): add money-display shared component

---
+++ src/components/shared/MoneyDisplay.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+  modelValue: boolean; // show amount
+  amount: number;
+  dotNumber: number;
+  label?: string;
+  visibilityToggle?: boolean;
+}>();
+
+defineEmits<{
+  (e: 'update:modelValue', val: boolean): void;
+}>();
+
+const displayContent = computed(() => {
+  return props.modelValue
+    ? props.amount.toLocaleString('en')
+    : Array(props.dotNumber).fill('•').join('');
+});
+</script>
+
+<template>
+  <div class="display">
+    <p class="display__content">
+      <span v-if="!!label">{{ label }}</span>
+      <span>{{ " $" }}</span>
+      <span class="display__amount">{{ displayContent }}</span>
+    </p>
+    <div
+      v-if="visibilityToggle"
+      class="display__visibility"
+    >
+      <QIcon
+        :name="modelValue ? 'o_visibility' : 'o_visibility_off'"
+        size="24px"
+        @click="$emit('update:modelValue', !modelValue)"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.display {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  &__content {
+    span {
+      @include text-style($headline-small, $on-surface);
+    }
+  }
+
+  // &__amount {
+  //   display: inline-block;
+  //   min-width: 120px;
+  // }
+
+  &__visibility {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 8px;
+    border-radius: 50%;
+    background-color: $secondary-container;
+    cursor: pointer;
+  }
+}
+</style>
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -38,3 +38,4 @@ export { default as SimpleSelect } from './SimpleSelect.vue';
 export { default as OImgPreview } from './OImgPreview.vue';
 export { default as BasicBtn } from './BasicBtn.vue';
 export { default as BasicTabs } from './BasicTabs.vue';
+export { default as MoneyDisplay } from './MoneyDisplay.vue';
