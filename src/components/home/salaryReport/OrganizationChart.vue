9f2aaedb1cd7a599aadaa0c0e31399a71390e56d
AJay <andrewli707577@gmail.com>
2025-05-15 15:05:31 +0800

    feat(salary-report): add organization chart component

---
+++ src/components/home/salaryReport/OrganizationChart.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+import ApexTree from 'apextree';
+import { nextTick, onMounted } from 'vue';
+
+interface Data {
+  content: {
+    label: string;
+    caption: string;
+  };
+  children?: Data[];
+}
+
+const props = defineProps<{
+  data: Data;
+}>();
+
+const options = {
+  nodeWidth: 117,
+  nodeHeight: 60,
+  childrenSpacing: 56,
+  siblingSpacing: 24,
+  direction: 'left',
+  borderRadius: '8px',
+  contentKey: 'content',
+  borderColor: 'transparent',
+  highlightOnHover: false,
+
+  nodeTemplate: (content: Data['content']) => {
+    return `<div class="node">
+      <div class="node__label">${content.label}</div>
+      <div class="node__caption">${content.caption}</div>
+    </div>`;
+  },
+};
+
+onMounted(async () => {
+  // This component is placed inside an expansion-item that is collapsed by default.
+  // We wait for the expansion-item to be opened before rendering the SVG,
+  // ensuring there's enough space available for proper SVG rendering.
+  await nextTick();
+
+  const tree = new ApexTree(document.getElementById('svg-tree'), options);
+  tree.render(props.data);
+
+  const svgEle = document.querySelector('#apexTreeWrapper > svg')!;
+
+  // remove viewBox to ensure the svg to fit the container
+  svgEle.removeAttribute('viewBox');
+
+  // The g element is not aligned with the left edge of the SVG container,
+  // so we apply a transform to adjust its position.
+  const gEle = svgEle.querySelector('g')!;
+  const distance = gEle.getBoundingClientRect().top - svgEle.getBoundingClientRect().top;
+  gEle.setAttribute('transform', `translate(0, ${-distance})`);
+
+  // Disable the default behavior that allows the SVG to be scrollable and zoomable.
+  svgEle.addEventListener('wheel', (e) => {
+    e.stopImmediatePropagation();
+  }, { capture: true });
+
+  // Disable the default behavior that allows the SVG to be scrollable and zoomable.
+  svgEle.addEventListener('mousedown', (e) => {
+    e.stopPropagation();
+  }, { capture: true });
+
+  // Resize the SVG to fit its container element.
+  const gEleRect = gEle.getBoundingClientRect();
+  svgEle.setAttribute('width', `${gEleRect.width}`);
+  svgEle.setAttribute('height', `${gEleRect.height}`);
+});
+</script>
+
+<template>
+  <div id="svg-tree" />
+</template>
+
+<style lang="scss" scoped>
+:deep(.node) {
+  background-color: #e1e5ec;
+  border-radius: 8px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+}
+:deep(.node__label) {
+  @include text-style($label-large-perminent, $on-surface);
+}
+
+:deep(.node__caption) {
+  @include text-style($body-small, $on-surface-variant);
+}
+:deep(svg path) {
+  stroke: #767680;
+}
+</style>
--- src/components/home/salaryReport/index.ts
+++ src/components/home/salaryReport/index.ts
@@ -1,2 +1,3 @@
 export { default as ExpansionItem } from './ExpansionItem.vue';
 export { default as MoneyDisplay } from './MoneyDisplay.vue';
+export { default as OrganizationChart } from './OrganizationChart.vue';
