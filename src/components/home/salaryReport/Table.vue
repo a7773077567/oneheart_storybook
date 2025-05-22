9b747d16822e9d0814281b1f7084e33301327b2f
AJay <andrewli707577@gmail.com>
2025-05-22 13:04:12 +0800

    feat(salary-report): add table component for salary-report

---
+++ src/components/home/salaryReport/Table.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <QTable
+    v-bind="$attrs"
+    class="table"
+    hide-pagination
+  >
+    <template
+      v-for="(_, name) in $slots"
+      :key="name"
+      #[name]="slotData"
+    >
+      <slot :name="name" v-bind="slotData" />
+    </template>
+  </QTable>
+</template>
+
+<style lang="scss" scoped>
+.table {
+  box-shadow: none;
+}
+
+:deep(th) {
+  @include text-style($label-large, $on-surface);
+}
+
+:deep(td) {
+  @include text-style($body-medium, $on-surface);
+}
+</style>
--- src/components/home/salaryReport/index.ts
+++ src/components/home/salaryReport/index.ts
@@ -1,3 +1,4 @@
 export { default as ExpansionItem } from './ExpansionItem.vue';
 export { default as MoneyDisplay } from './MoneyDisplay.vue';
 export { default as OrganizationChart } from './OrganizationChart.vue';
+export { default as Table } from './Table.vue';
