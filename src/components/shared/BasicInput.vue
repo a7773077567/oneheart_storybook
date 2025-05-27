c5872df575c4778c1b40ae4595961cdd5c294cc3
AJay <andrewli707577@gmail.com>
2025-05-27 23:34:56 +0800

    feat(component): add basic-input shared component

---
+++ src/components/shared/BasicInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { QInputProps } from 'quasar';
+
+const props = defineProps<QInputProps>();
+</script>
+
+<template>
+  <QInput
+    v-bind="props"
+    outlined
+    class="input"
+  />
+</template>
+
+<style lang="scss" scoped>
+.input {
+  :deep(.q-field__control) {
+    border-radius: 8px;
+  }
+  :deep(.q-field__control:before) {
+    border: 1px solid $outline;
+  }
+  :deep(.q-field__label) {
+    @include text-style($body-small, $on-surface-variant);
+  }
+  :deep(.q-field__native) {
+    @include text-style($body-large, $on-surface);
+  }
+}
+</style>
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -40,3 +40,4 @@ export { default as BasicBtn } from './BasicBtn.vue';
 export { default as BasicTabs } from './BasicTabs.vue';
 export { default as MoneyDisplay } from '../home/salaryReport/MoneyDisplay.vue';
 export { default as YearMonthSelect } from './YearMonthSelect.vue';
+export { default as BasicInput } from './BasicInput.vue';
