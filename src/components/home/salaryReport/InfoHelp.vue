5c43e2c9c7992c9de1223850d8c84f95bc482deb
AJay <andrewli707577@gmail.com>
2025-06-28 23:01:41 +0800

    feat(salary-report): add info-help component

---
+++ src/components/home/salaryReport/InfoHelp.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  infos: string[];
+  showHelpIcon?: boolean;
+  tooltip?: string;
+}>();
+</script>
+
+<template>
+  <div class="info-hint">
+    <QIcon name="o_info" size="20px" />
+    <span
+      v-for="(info, idx) in infos"
+      :key="idx"
+    >{{ info }}</span>
+    <QIcon v-if="showHelpIcon" name="o_help_outline" size="20px" />
+
+    <QTooltip v-if="tooltip" max-width="320px" class="bg-black" style="white-space: pre-wrap;">
+      <span class="text-body-small">{{ tooltip }}</span>
+    </QTooltip>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.info-hint {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  > span {
+    @include text-style($body-medium, $on-surface-variant);
+  }
+}
+</style>
--- src/components/home/salaryReport/index.ts
+++ src/components/home/salaryReport/index.ts
@@ -2,3 +2,4 @@ export { default as ExpansionItem } from './ExpansionItem.vue';
 export { default as MoneyDisplay } from './MoneyDisplay.vue';
 export { default as OrganizationChart } from './OrganizationChart.vue';
 export { default as Table } from './Table.vue';
+export { default as InfoHelp } from './InfoHelp.vue';
